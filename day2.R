7dae7c9d9a0f8a2897a2b3f1e23f8ab7d631f6a6
tinigarske <t.garske@imperial.ac.uk>
2017-09-27 14:16:39 +0100

    further little commands for creating vignettes and running knitr

--- day2.R
+++ day2.R
@@ -7,7 +7,10 @@ library("nclRshiny")
 
 vignette("slides1", package = "nclRshiny")
 vignette("chapter1", package = "nclRshiny")
+vignette("chapter2", package = "nclRshiny")
+vignette("chapter3", package = "nclRshiny")
 
 nclRshiny::create_project()
 ## some editing of the .Rmd
-rmarkdown::render("exercises/chapter1/markdown1.Rmd")
+rmarkdown::render("exercises/chapter1/markdown1.Rmd""")
+rmarkdown::render("test.Rmd")
